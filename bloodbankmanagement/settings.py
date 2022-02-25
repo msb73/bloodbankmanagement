6d061f5f9d0c12930d5bc9aab7afaad1897efed2
msb73 <68978919+msb73@users.noreply.github.com>
2022-02-25 20:59:11 +0530

    Update settings.py

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR = os.path.join(BASE_DIR,'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -122,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'db.sqlite3'),
 ]
 LOGIN_REDIRECT_URL='/afterlogin'
 
